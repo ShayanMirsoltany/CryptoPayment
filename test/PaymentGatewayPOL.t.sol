54d0eb4b00f081b98c8cd062449fc345360e1599
shayan <shayan@SOLTANI-SH>
2025-12-13 12:23:21 +0330

    erc20 test completed

--- test/PaymentGatewayPOL.t.sol
+++ test/PaymentGatewayPOL.t.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.20;
 import "@utils/Roles.sol";
 import "@utils/Errors.sol";
 import "@erc20/CLT_Token.sol";
+import "@core/Gateways/PaymentGatewayPOL.sol";
+import "@core/OrdersInWait.sol";
+
 import { Test, console } from "forge-std/Test.sol";
 import "@openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
 contract PaymentGatewayPOLTest is Test {}
